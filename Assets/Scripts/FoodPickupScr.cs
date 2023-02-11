42b05ee9b1299ae90b2145d28c19bbc98da34ec9
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-02-11 13:59:38 -0800

    Added food scripts and home scripts with movement

---
+++ Assets/Scripts/FoodPickupScr.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodPickupScr : MonoBehaviour
+{
+    bool canpickup;
+    public int foodItems = 0;
+    public int maxItems;
+    GameObject ObjectIwantToPickUp;
+    public bool canLeave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (foodItems > maxItems)
+        {
+            canLeave = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if(other.gameObject.tag == "Food")
+        {
+            Debug.Log("HOW");
+            canpickup = true;  
+            ObjectIwantToPickUp = other.gameObject; 
+            foodItems += 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
