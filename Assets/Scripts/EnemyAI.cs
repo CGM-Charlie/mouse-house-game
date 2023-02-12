4e6191f4aae8bcf8ad43380202bb7dd77175305a
CDaniaN <72101575+CDaniaN@users.noreply.github.com>
2023-02-12 09:57:09 -0800

    AI update

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
+    public GameObject room1;
+    public GameObject room2;
+    public GameObject room3;
+    public GameObject room4;
+    public GameObject room5;
     public float speed;
     public float distanceBetween;
 
@@ -23,10 +28,39 @@ public class EnemyAI : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if(distance < distanceBetween)
+        if(distance < distanceBetween) //distance < 0 for always chase
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed = Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+        else
+        {
+            int roomNum = Random.Range(1,6);
+            if(roomNum == 1)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, room1.transform.position, speed = Time.deltaTime);
+                transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            }
+            if(roomNum == 2)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, room2.transform.position, speed = Time.deltaTime);
+                transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            }
+            if(roomNum == 3)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, room3.transform.position, speed = Time.deltaTime);
+                transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            }
+            if(roomNum == 4)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, room4.transform.position, speed = Time.deltaTime);
+                transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, room5.transform.position, speed = Time.deltaTime);
+                transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            }
         }
     }
 }
